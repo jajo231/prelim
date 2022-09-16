66f8539d8a095a380e4660d0544e23e9578d6041
unknown <sumipo231@gmail.com>
2022-09-16 11:24:18 +0800

    First commit

---
+++ resources/views/Homepage.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <link rel= "stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+<body>
+<div class="text-center">
+        <nav class="nav nav-tabs">
+            <a href="{{url('/')}}" class="nav-link active">Home</a>
+            <a href="{{url('/air')}}" class="nav-link">Page Info</a>
+            <a href="{{url('/hangin')}}" class="nav-link">Terms of Use</a>
+        </nav>
+        <h1><i>Art Example</i></h1>
+        <p>
+        <img src="https://allthatsinteresting.com/thumb/1200.633.https://allthatsinteresting.com/wordpress/wp-content/uploads/2013/09/interesting-pictures-of-clouds.jpg" class="img-thumbnail" style="width:700px;height:300px;" alt="Image">
+        </p>
+    
+</body>
+</html>
